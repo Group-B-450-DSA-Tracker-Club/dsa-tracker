900eba0dabaf2ce58800d8dd2491c5da3bc06697
Kevin <kevin.chang@revature.net>
2021-08-16 10:59:04 -0400

    setup a questions.ts file with proper interfaces

---
+++ 450dsatracker/src/database/questions.ts
@@ -0,0 +1,245 @@
+interface IQuestion {
+    Topic: string;
+    Problem: string;
+    Done: boolean;
+    URL: string;
+}
+
+interface IQuestionData {
+    topicName: string;
+    position: number;
+    started: boolean;
+    doneQuestions: number;
+    questions: IQuestion[];
+}
+
+
+export const questions: IQuestionData = {
+    topicName: "Array",
+    position: 0,
+    started: false,
+    doneQuestions: 0,
+    questions: [
+        {
+            Topic: "Array",
+            Problem: "Reverse the array",
+            Done: false,
+            URL: "https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/",
+        },
+        {
+            Topic: "Array",
+            Problem: "Find the maximum and minimum element in an array",
+            Done: false,
+            URL: "https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/",
+        },
+        {
+            Topic: "Array",
+            Problem: 'Find the "Kth" max and min element of an array ',
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/kth-smallest-element/0",
+        },
+        {
+            Topic: "Array",
+            Problem:
+                "Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Move all the negative elements to one side of the array ",
+            Done: false,
+            URL: "https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/",
+        },
+        {
+            Topic: "Array",
+            Problem: "Find the Union and Intersection of the two sorted arrays.",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/union-of-two-arrays/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Write a program to cyclically rotate an array by one.",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "find Largest sum contiguous Subarray [V. IMP]",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Minimise the maximum difference between heights [V.IMP]",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/minimize-the-heights3351/1",
+        },
+        {
+            Topic: "Array",
+            Problem: "Minimum no. of Jumps to reach end of an array",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "find duplicate in an array of N+1 Integers",
+            Done: false,
+            URL: "https://leetcode.com/problems/find-the-duplicate-number/",
+        },
+        {
+            Topic: "Array",
+            Problem: "Merge 2 sorted arrays without using Extra space.",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays5135/1",
+        },
+        {
+            Topic: "Array",
+            Problem: "Kadane's Algo [V.V.V.V.V IMP]",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Merge Intervals",
+            Done: false,
+            URL: "https://leetcode.com/problems/merge-intervals/",
+        },
+        {
+            Topic: "Array",
+            Problem: "Next Permutation",
+            Done: false,
+            URL: "https://leetcode.com/problems/next-permutation/",
+        },
+        {
+            Topic: "Array",
+            Problem: "Count Inversion",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/inversion-of-array/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Best time to buy and Sell stock",
+            Done: false,
+            URL: "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/",
+        },
+        {
+            Topic: "Array",
+            Problem:
+                "find all pairs on integer array whose sum is equal to given number",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1",
+        },
+        {
+            Topic: "Array",
+            Problem: "find common elements In 3 sorted arrays",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/common-elements1132/1",
+        },
+        {
+            Topic: "Array",
+            Problem:
+                "Rearrange the array in alternating positive and negative items with O(1) extra space",
+            Done: false,
+            URL: "https://www.geeksforgeeks.org/rearrange-array-alternating-positive-negative-items-o1-extra-space/",
+        },
+        {
+            Topic: "Array",
+            Problem: "Find if there is any subarray with sum equal to 0",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/subarray-with-0-sum/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Find factorial of a large number",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/factorials-of-large-numbers/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "find maximum product subarray ",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/maximum-product-subarray3604/1",
+        },
+        {
+            Topic: "Array",
+            Problem: "Find longest coinsecutive subsequence",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/longest-consecutive-subsequence/0",
+        },
+        {
+            Topic: "Array",
+            Problem:
+                'Given an array of size n and a number k, fin all elements that appear more than " n/k " times.',
+            Done: false,
+            URL: "https://www.geeksforgeeks.org/given-an-array-of-of-size-n-finds-all-the-elements-that-appear-more-than-nk-times/",
+        },
+        {
+            Topic: "Array",
+            Problem: "Maximum profit by buying and selling a share atmost twice",
+            Done: false,
+            URL: "https://www.geeksforgeeks.org/maximum-profit-by-buying-and-selling-a-share-at-most-twice/",
+        },
+        {
+            Topic: "Array",
+            Problem: "Find whether an array is a subset of another array",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/array-subset-of-another-array/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Find the triplet that sum to a given value",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/triplet-sum-in-array/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Trapping Rain water problem",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/trapping-rain-water/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Chocolate Distribution problem",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Smallest Subarray with sum greater than a given value",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/smallest-subarray-with-sum-greater-than-x/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Three way partitioning of an array around a given value",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/three-way-partitioning/1",
+        },
+        {
+            Topic: "Array",
+            Problem: "Minimum swaps required bring elements less equal K together",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/minimum-swaps-required-to-bring-all-elements-less-than-or-equal-to-k-together/0",
+        },
+        {
+            Topic: "Array",
+            Problem:
+                "Minimum no. of operations required to make an array palindrome",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/palindromic-array/0",
+        },
+        {
+            Topic: "Array",
+            Problem: "Median of 2 sorted arrays of equal size",
+            Done: false,
+            URL: "https://practice.geeksforgeeks.org/problems/find-the-median0527/1",
+        },
+        {
+            Topic: "Array",
+            Problem: "Median of 2 sorted arrays of different size",
+            Done: false,
+            URL: "https://www.geeksforgeeks.org/median-of-two-sorted-arrays-of-different-sizes/",
+        },
+    ],
+}
